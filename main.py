90a651af50593bc43ce5a97faa77ba0110dd2e36
acturus <82964583+acturus1@users.noreply.github.com>
2024-12-02 17:13:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
+from PyQt5 import QtWidgets, QtGui
 import sys
 import random
-from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QColor
+
+
+class Ui_MainWindow:
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(500, 500)
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.drawButton = QtWidgets.QPushButton(self.centralwidget)
+        self.drawButton.setObjectName("drawButton")
+        self.drawButton.setText("нарисоват")
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi('UI.ui', self)
-        self.drawButton.clicked.connect(self.draw_circle)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.drawButton.clicked.connect(self.draw_circle)
 
         self.circles = []
 
     def draw_circle(self):
-        diameter = random.randint(1, 100)
-        self.circles.append(diameter)
+        diameter = random.randint(10, 100)
+        color = self.random_color()
+        self.circles.append((diameter, color))
         self.update()
 
+    def random_color(self):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        return (r, g, b)
+
     def paintEvent(self, event):
-        painter = QPainter(self)
-        for diameter in self.circles:
+        painter = QtGui.QPainter(self)
+        for diameter, color in self.circles:
             x = random.randint(0, self.width() - diameter)
             y = random.randint(0, self.height() - diameter)
-            painter.setBrush(QColor('yellow'))
+            painter.setBrush(QtGui.QColor(*color))
             painter.drawEllipse(x, y, diameter, diameter)
 
 if __name__ == '__main__':
